7b2373797e09f127c975407de276bf3db32e00b8
Andreas Frisch <andreas.frisch@gmail.com>
2014-06-19 14:46:27 +0200

    Started development in dedicated branch Removed initial standard files Implemented first test and corresponding dummy rover

---
+++ mars_rover/src/main/java/com/metube/app/Rover.java
@@ -0,0 +1,17 @@
+package com.metube.app;
+
+/**
+ * Dummy Rover
+ *
+ */
+public class Rover {
+	private String heading;
+	private int x_coordinate;
+	private int y_coordinate;
+
+	Rover (int x, int y, String heading) {
+		heading = heading;
+		x_coordinate = x;
+		y_coordinate = y;
+	}
+}
---
+++ mars_rover/src/test/java/com/metube/app/RoverTest.java
@@ -0,0 +1,36 @@
+package com.metube.app;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RoverTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RoverTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(RoverTest.class);
+    }
+
+    /**
+     * Testing Rover creation and initial location
+     */
+    public void testRoverCreation() {
+    	Rover rover = new Rover(0,0,"N");
+        assertTrue(rover.getHeading() == "N");
+        assertTrue(rover.getXCoordinate() == 0);
+        assertTrue(rover.getYCoordinate() == 0);
+    }
+}
