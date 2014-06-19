126f9a7b877e5c8143c9fabe8434845ff6b69bb2
Andreas Frisch <andreas.frisch@gmail.com>
2014-06-19 17:47:28 +0200

    Added tests for remaining movement directions

--- mars_rover/src/test/java/com/metube/app/RoverTest.java
+++ mars_rover/src/test/java/com/metube/app/RoverTest.java
@@ -69,4 +69,34 @@ public class RoverTest extends TestCase {
     	assertEquals(rover.get_x_coordinate(),0);
     	assertEquals(rover.get_heading(),"N");
     }
+    public void testRoverMoveAheadSouth() {
+    	Rover rover = new Rover(0,3,"S");
+    	assertEquals(rover.get_y_coordinate(),3);
+    	assertEquals(rover.get_x_coordinate(),0);
+    	assertEquals(rover.get_heading(),"S");
+    	rover.execute_order("M");
+    	assertEquals(rover.get_y_coordinate(),2);
+    	assertEquals(rover.get_x_coordinate(),0);
+    	assertEquals(rover.get_heading(),"S");
+    }
+    public void testRoverMoveAheadEast() {
+    	Rover rover = new Rover(0,0,"E");
+    	assertEquals(rover.get_y_coordinate(),0);
+    	assertEquals(rover.get_x_coordinate(),0);
+    	assertEquals(rover.get_heading(),"E");
+    	rover.execute_order("M");
+    	assertEquals(rover.get_y_coordinate(),0);
+    	assertEquals(rover.get_x_coordinate(),1);
+    	assertEquals(rover.get_heading(),"E");
+    }
+    public void testRoverMoveAheadWest() {
+    	Rover rover = new Rover(1,0,"W");
+    	assertEquals(rover.get_y_coordinate(),0);
+    	assertEquals(rover.get_x_coordinate(),1);
+    	assertEquals(rover.get_heading(),"W");
+    	rover.execute_order("M");
+    	assertEquals(rover.get_y_coordinate(),0);
+    	assertEquals(rover.get_x_coordinate(),0);
+    	assertEquals(rover.get_heading(),"W");
+    }
 }
