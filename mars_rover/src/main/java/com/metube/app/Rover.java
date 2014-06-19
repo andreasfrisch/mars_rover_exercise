a8cdbfd103609f743c8fa3c0dc98202fd4ce86b6
Andreas Frisch <andreas.frisch@gmail.com>
2014-06-19 15:45:53 +0200

    Added required getters to Rover.java Fixed naming issue rgrd scoping

--- mars_rover/src/main/java/com/metube/app/Rover.java
+++ mars_rover/src/main/java/com/metube/app/Rover.java
@@ -1,17 +1,26 @@
 package com.metube.app;
 
 /**
- * Dummy Rover
- *
+ * Rover
  */
 public class Rover {
 	private String heading;
 	private int x_coordinate;
 	private int y_coordinate;
 
-	Rover (int x, int y, String heading) {
-		heading = heading;
+	Rover (int x, int y, String h) {
+		heading = h;
 		x_coordinate = x;
 		y_coordinate = y;
+	}
+
+	public String get_heading() {
+		return heading;
+	}
+	public int get_x_coordinate() {
+		return x_coordinate;
+	}
+	public int get_y_coordinate() {
+		return y_coordinate;
 	}
 }
