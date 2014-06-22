6f75b3b079ecfe0dae777ebbfef02d2ade705ca8
Andreas Frisch <andreas.frisch@gmail.com>
2014-06-22 16:18:53 +0200

    Updated controller to run simulation Not including commands

--- mars_rover/src/main/java/com/metube/app/Controller.java
+++ mars_rover/src/main/java/com/metube/app/Controller.java
@@ -5,11 +5,60 @@ package com.metube.app;
  */
 public class Controller {
 	private String filepath = "";
+	private RoverInstructionFileParser parser;
+	private Plateau plateau;
+	private Rover rover1;
+	private Rover rover2;
+
 	public static void main(String[] args) {
-		if (args.length == 1) {
-			System.out.println("You requested: "+args[0]);
+		Controller controller = new Controller();
+		controller.start(args);
+	}	
+	public void start(String[] args) {
+		if (args.length == 0) {
+			filepath = "core_test.txt";
+		} else if (args.length == 1) {
+			filepath = args[0];
 		} else {
 			System.out.println("Usage: Please specify filepath...");
 		}
+
+		parser = new RoverInstructionFileParser(filepath);
+		String[] plateau_dimensions = parser.tokenize(parser.get_line());
+		plateau = new Plateau(
+				Integer.parseInt(plateau_dimensions[0]),
+				Integer.parseInt(plateau_dimensions[1])
+		);
+		String[] rover1_init = parser.tokenize(parser.get_line());
+		rover1 = new Rover(
+				Integer.parseInt(rover1_init[0]),
+				Integer.parseInt(rover1_init[1]),
+				rover1_init[2]
+		);
+		//move rover 1
+		String rover1_commands = parser.get_line();
+
+		String[] rover2_init = parser.tokenize(parser.get_line());
+		rover2 = new Rover(
+				Integer.parseInt(rover2_init[0]),
+				Integer.parseInt(rover2_init[1]),
+				rover2_init[2]
+		);
+		//move rover 2
+		String rover2_commands = parser.get_line();
+
+		//finally
+		//output new rover coordinates and heading
+		//TODO: use String.format when upgrading java version
+		String position1 = "Rover 1: "+
+				rover1.get_x_coordinate()+" "+
+				rover1.get_y_coordinate()+" "+
+				rover1.get_heading();
+		System.out.println(position1);
+		String position2 = "Rover 2: "+
+				rover2.get_x_coordinate()+" "+
+				rover2.get_y_coordinate()+" "+
+				rover2.get_heading();
+		System.out.println(position2);
 	}
 }
